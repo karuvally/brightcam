4db23f276c794bdffba6b4eba196b92dc512ad09
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2020-02-04 21:45:01 +0530

    added some headers

---
+++ brightcam.c
@@ -0,0 +1,17 @@
+/*
+ * brightcam
+ * Copyright 2020, Aswin Babu Karuvally (aswinbabuk@gmail.com)
+ */
+
+// include serious stuff
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<linux/videodev2.h>
+
+
+// the main function
+int main()
+{
+	return 0;
+}
